076fafe3217de688b5739552643b9208f94111dc
Duany Dreyton <duanyrf@gmail.com>
2023-10-26 19:57:06 -0300

    initial commit

--- App.tsx
+++ App.tsx
@@ -4,7 +4,14 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <View style={styles.parteSuperior}>
+        <Text>Parte Superior</Text>
+      </View>
+
+      <View style={styles.parteInferior}>
+        <Text>Parte Inferior</Text>
+      </View>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +21,22 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    // alignItems: 'center',
+    // justifyContent: 'center',
+  },
+  parteSuperior: {
+    flex: 0.30,
+    width: '100%',
+    backgroundColor: '#ecfb68',
+    paddingTop: 50,
     alignItems: 'center',
     justifyContent: 'center',
   },
+  parteInferior: {
+    flex: 1,
+    width: '100%',
+    backgroundColor: '#8a68fb',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
 });
